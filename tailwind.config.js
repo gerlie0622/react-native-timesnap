3f7e452ab0489414e9da52ae7902bc594bd74544
Gabriel Alfonso Mapa <gabrielalfonsomapa@gmail.com>
2023-12-05 03:55:51 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Camera.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
