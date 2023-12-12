d037ef3b3d260861293fae55db0e38ca7125bebc
gerlie0622 <gerliedelacruz54@gmail.com>
2023-12-12 11:29:57 +0800

    timeEntries

--- screens/Attendance.js
+++ screens/Attendance.js
@@ -105,7 +105,7 @@ const Attendance = () => {
     console.log('Saving time to Firestore...');
 
     try {
-      const timeEntriesRef = collection(dbFirestore, 'timeEntriesDraft');
+      const timeEntriesRef = collection(dbFirestore, 'timeEntries');
 
       let duration = null;
       if (eventType === 'Time Out' && timeInTimestamp) {
