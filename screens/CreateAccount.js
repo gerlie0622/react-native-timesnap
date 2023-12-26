6cef3d80703e841643a1310e713709950cb334ea
gerlie0622 <gerliedelacruz54@gmail.com>
2023-12-26 22:16:00 +0800

    create account update

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { auth, dbFirestore } from '../firebase';
 import { collection, addDoc } from 'firebase/firestore';
@@ -13,15 +13,11 @@ const CreateAccount = () => {
   const [password, setPassword] = useState('');
   const [type, setType] = useState('employee');
 
-
   const handleCreateAccount = async () => {
-    console.log('Starting handleCreateAccount');
     try {
-      // Create a new user in Firebase Authentication
       const userCredential = await auth.createUserWithEmailAndPassword(email, password);
       const newUser = userCredential.user;
-  
-      // Save user details to Firestore
+
       const usersRef = collection(dbFirestore, 'users');
       const newUserDocRef = await addDoc(usersRef, {
         uid: newUser.uid,
@@ -33,9 +29,6 @@ const CreateAccount = () => {
         type,
       });
 
-  
-      console.log('User registered successfully with ID:', newUserDocRef.id);
-
       setName('');
       setEmail('');
       setContactNumber('');
@@ -43,87 +36,89 @@ const CreateAccount = () => {
       setSchedule('opening');
       setPassword('');
       setType('employee');
-      
-      // Additional actions after successful registration
+
       Alert.alert('Account Created!', 'Your account has been created successfully.', [
         { text: 'OK', onPress: () => console.log('OK Pressed') },
       ]);
-  
     } catch (error) {
       console.error('Error creating account:', error.message);
-      // Handle error (e.g., show an alert)
     }
-    console.log('Finished handleCreateAccount');
   };
 
   return (
     <View style={styles.container}>
-      <Text>Create Account</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        value={name}
-        onChangeText={(text) => setName(text)}
+      <Image
+        source={require('../assets/logo.png')} // Replace with the path to your background image
+        style={styles.backgroundImage}
       />
+      <View style={styles.overlay} />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
+      <View style={styles.card}>
+        <Text style={styles.title}>Create Account</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Contact Number"
-        value={contactNumber}
-        onChangeText={(text) => setContactNumber(text)}
-      />
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
 
-      <Text>Select Salary:</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={salary}
-        onValueChange={(itemValue, itemIndex) => setSalary(itemValue)}
-      >
-        <Picker.Item label="P 620" value="620" />
-        <Picker.Item label="P 720" value="720" />
-        {/* Add other salary options as needed */}
-      </Picker>
+        <TextInput
+          style={styles.input}
+          placeholder="Email Address"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
 
-      <Text>Select Schedule:</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={schedule}
-        onValueChange={(itemValue, itemIndex) => setSchedule(itemValue)}
-      >
-        <Picker.Item label="Opening" value="opening" />
-        <Picker.Item label="Closing" value="closing" />
-      </Picker>
+        <TextInput
+          style={styles.input}
+          placeholder="Contact Number"
+          value={contactNumber}
+          onChangeText={(text) => setContactNumber(text)}
+        />
 
-      <Text>Select Type:</Text>
-      <Picker
-        style={styles.picker}
-        selectedValue={type}
-        onValueChange={(itemValue, itemIndex) => setType(itemValue)}
-      >
-        <Picker.Item label="Employee" value="employee" />
-        <Picker.Item label="Admin" value="admin" />
-      </Picker>
+        <Text style={styles.label}>Select Salary:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={salary}
+          onValueChange={(itemValue) => setSalary(itemValue)}
+        >
+          <Picker.Item label="P 620" value="620" />
+          <Picker.Item label="P 720" value="720" />
+        </Picker>
 
+        <Text style={styles.label}>Select Schedule:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={schedule}
+          onValueChange={(itemValue) => setSchedule(itemValue)}
+        >
+          <Picker.Item label="Opening" value="opening" />
+          <Picker.Item label="Closing" value="closing" />
+        </Picker>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
+        <Text style={styles.label}>Select Type:</Text>
+        <Picker
+          style={styles.picker}
+          selectedValue={type}
+          onValueChange={(itemValue) => setType(itemValue)}
+        >
+          <Picker.Item label="Employee" value="employee" />
+          <Picker.Item label="Admin" value="admin" />
+        </Picker>
 
-      <TouchableOpacity onPress={handleCreateAccount} style={styles.button}>
-        <Text style={styles.buttonText}>Sign Up</Text>
-      </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+
+        <TouchableOpacity onPress={handleCreateAccount} style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -134,26 +129,62 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    resizeMode: 'cover',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    width: '80%',
+    elevation: 3,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: 'black',
+  },
   input: {
     height: 40,
-    width: '80%',
     borderColor: 'gray',
     borderWidth: 1,
     marginVertical: 10,
     paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: 'white',
   },
   picker: {
     height: 40,
-    width: '80%',
     borderColor: 'gray',
     borderWidth: 1,
     marginVertical: 10,
+    borderRadius: 5,
+    backgroundColor: 'white',
+  },
+  label: {
+    fontSize: 16,
+    marginVertical: 10,
+    color: 'black',
   },
   button: {
-    backgroundColor: '#0782F9',
-    width: '60%',
+    backgroundColor: '#1877f2', // Facebook color
     padding: 15,
-    borderRadius: 10,
+    borderRadius: 5,
     alignItems: 'center',
     marginTop: 20,
   },
